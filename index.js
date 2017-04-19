21155267de0c48c75ff0479147d42f02dae5b4b4
Anthony Super <anthony@noided.media>
2017-04-19 14:55:47 -0600

    Add Babel

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log("Hello, world!");
+let log = (e) => {
+  console.log(`Got message: "${e}"`);
+}
+
+log("This is babel!");
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   entry: './index.js',
+  module: {
+    rules: [{
+      test: /\.(js|jsx)/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ["react", "latest"]
+        }
+      }
+    }]
+  },
   output: {
     path: __dirname + "/build",
     filename: "bundle.js"
